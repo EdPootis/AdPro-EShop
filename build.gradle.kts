de92a06ebe2921dad6e0eb8dcf540353fc2c7fd9
Edmond Christian <edmond.chr@gmail.com>
2025-02-18 23:58:28 +0700

    build: added SonarCloud workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,10 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.0.1.5171"
 }
+
+
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
@@ -76,4 +79,12 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "EdPootis_AdPro-EShop")
+        property("sonar.organization", "edpootis")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
