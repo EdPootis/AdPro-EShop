671104d7e3cb0bf394478cd6f88e65eeda3b8bc6
Edmond Christian <edmond.chr@gmail.com>
2025-02-18 11:17:49 +0700

    test: added jacocoTestReport

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateEditDeleteProductFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateEditDeleteProductFunctionalTest.java
@@ -1,7 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.functional;
 
 import io.github.bonigarcia.seljup.SeleniumJupiter;
-import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,7 +18,7 @@ import java.util.List;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ExtendWith(SeleniumJupiter.class)
-class CreateEditDeleteProductTest {
+class CreateEditDeleteProductFunctionalTest {
     /**
      * The port number assigned to the running application during test execution.
      * Set automatically during each test run by Spring Framework's test context.
