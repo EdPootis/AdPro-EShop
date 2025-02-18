5e018fbe7c024d0232b0eac6ade23000811e70f8
Edmond Christian <edmond.chr@gmail.com>
2025-02-18 23:37:40 +0700

    test: added controller unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomePageControllerTest.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(MainController.class)
+class HomePageControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomePage() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/"))
+                .andReturn().getResponse();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+}
